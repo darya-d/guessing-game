9088a52b57649bf2f5f3c778b682b68de4e036b8
Darya <darya.drazdouskaya@gmail.com>
2019-10-11 19:31:37 +0300

    feat: implement GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
+/**
+ * GuessingGame - implement binary search
+ */
+
 class GuessingGame {
     constructor() {}
-
+    // setRange(min, max) - range of numbers from min and max
+    // 'this.' - reference to the current object 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
+    // split range in half using Binary search
     guess() {
-
+        this.half = Math.round(this.min + (this.max - this.min)/2);
+        return this.half;
     }
 
+    // looking in the smaller part of range
     lower() {
-
+        this.max = this.half;
     }
 
+    // looking for the bigger part of range
     greater() {
-
+        this.min = this.half;
     }
 }
 
